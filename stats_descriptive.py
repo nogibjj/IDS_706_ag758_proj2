c38fa2e79b8603c84508e93ce7dcb89544f85d2d
ayushg245 <143531654+ayushg245@users.noreply.github.com>
2023-09-10 18:43:15 -0400

    Create stats_descriptive.py

---
+++ stats_descriptive.py
@@ -0,0 +1,14 @@
+def stats_mean(df):
+    return df['age'].mean()
+  
+
+def stats_median(df):
+    return df["age"].median()
+  
+
+def stats_mode(df):
+    return df["age"].mode()
+  
+
+def stats_std(df):
+    return df["age"].std()
