1c421b6cb5b055b96f9d5ef49f4d45d6b5a71a17
ayushg245 <143531654+ayushg245@users.noreply.github.com>
2023-09-10 19:56:16 -0400

    Update test_stats.py

--- test_stats.py
+++ test_stats.py
@@ -1,13 +1,13 @@
-from stats_descriptive import stats_mean, stats_median, stats_mode, stats_std
+import stats_descriptive
 import pandas as pd
 
 data = pd.read_csv("forbes_2022_billionaires.csv")
 
 def test_values():
-    assert (stats_mean(data)) == 64.21068938807126
-    assert (stats_median(data)) == 64.0
-    assert (stats_mode(data)[0]) == 57
-    assert (stats_std(data)) == 13.401258058138897
+    assert (stats_descriptive.stats_mean(data)) == 64.21068938807126
+    assert (stats_descriptive.stats_median(data)) == 64.0
+    assert (stats_descriptive.stats_mode(data)[0]) == 57
+    assert (stats_descriptive.stats_std(data)) == 13.401258058138897
 
 
 test_values()
