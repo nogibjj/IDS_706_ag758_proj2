f68f70aeceba64fe8caaa065e4bfd138663c82f2
ayushg245 <143531654+ayushg245@users.noreply.github.com>
2023-09-10 23:02:52 -0400

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
+
+df = pd.read_csv("World University Rankings 2023.csv")
+
 grouped = df.groupby('country')['finalWorth'].mean().reset_index()
 plt.bar(grouped['country'], grouped['finalWorth'])
 plt.xlabel('country')
